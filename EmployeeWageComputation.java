f9fb8b5fe1c371c53ed4233b71c7fe2bd5851074
pavan0628 <pavan.cse.rymec@gmail.com>
2025-04-12 11:50:04 +0530

    Added a random method to check if the employee is abs…

--- EmployeeWageComputation.java
+++ EmployeeWageComputation.java
@@ -1,11 +1,24 @@
 package com.bl.oops;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class EmployeeWageComputation {
 
-	//Main method
+	// Main method
 	public static void main(String[] args) {
-
-		//Display welcome message
-		System.out.println("Welcome to Employee Wage Computation Problem!");
+		// Displaying a welcome message
+		System.out.println("Welcome to employee wage computation Problem!!");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a employee Name : ");
+		String name = sc.nextLine();
+		Random random = new Random();
+		int attendance = random.nextInt(2);
+		System.out.println(attendance);
+		if (attendance == 0) {
+			System.out.println("Employee " + name + " is absent!!");
+		} else
+			System.out.println("Employee " + name + " is presnt ");
+		sc.close()
 	}
 }
