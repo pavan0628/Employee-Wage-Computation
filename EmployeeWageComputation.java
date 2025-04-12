f60e29a2657beb86a06dd69f5bcbcc612cce478c
pavan0628 <pavan.cse.rymec@gmail.com>
2025-04-12 11:37:44 +0530

    Display Welcome message for EmployeeWageComputation problem

---
+++ EmployeeWageComputation.java
@@ -0,0 +1,11 @@
+package com.bl.oops;
+
+public class EmployeeWageComputation {
+
+	//Main method
+	public static void main(String[] args) {
+
+		//Display welcome message
+		System.out.println("Welcome to Employee Wage Computation Problem!");
+	}
+}
